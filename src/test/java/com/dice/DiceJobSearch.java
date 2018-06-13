b8715642057eeb9cad6826732a6fd4befe9fa992
nevaselma <hurigulsum@gmail.com>
2018-06-13 17:36:09 -0500

    print test and timestamp

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,5 +1,6 @@
 package com.dice;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -55,5 +56,6 @@ public class DiceJobSearch {
 		}
 		
 		driver.close();
+		System.out.println("TEST COMPLETED - " + LocalDateTime.now());
 	}
 }
